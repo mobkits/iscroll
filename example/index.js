6f225c96bdc05409d154ee95973cb84b279c159b
chemzqm <chemzqm@gmail.com>
2015-11-09 00:54:16 +0800

    improve example works on desktop

--- example/index.js
+++ example/index.js
@@ -1,38 +1,36 @@
 require('../iscroll.css')
-if ('ontouchstart' in window) {
-  document.getElementById('mobile').style.display = 'none'
-}
-var iscroll = require('..')
-var el = document.querySelector('.scrollable')
-
-var is = iscroll(el, {
-  handlebar: true,
-  autorefresh: false
-})
 var log = document.getElementById('log')
-
-//is.on('scroll', function(y) {
-//  log.textContent = y
-//}, false)
-
-el.addEventListener('scroll', function () {
-  log.textContent = el.scrollTop
-})
-
-is.on('scrollend', function(e) {
-  console.log(e)
-})
-
 document.getElementById('add').addEventListener('click', function(e) {
   var c = el.querySelector('.content')
   for (var i = 1; i < 5; i++) {
     var node = document.createElement('li')
     node.textContent = i
     c.appendChild(node)
-    is.refresh()
+    if (is) is.refresh()
   }
 }, false)
 
 document.getElementById('top').addEventListener('click', function(e) {
-  is.scrollTo(0, 200)
+  el.scrollTop = 0
 }, false)
+
+var el = document.querySelector('.scrollable')
+el.addEventListener('scroll', function () {
+  log.textContent = el.scrollTop
+})
+
+if ('ontouchstart' in window) {
+  document.getElementById('mobile').style.display = 'none'
+  var iscroll = require('..')
+
+  var is = iscroll(el, {
+    handlebar: true,
+    autorefresh: false
+  })
+
+  is.on('scrollend', function(e) {
+    //console.log(e)
+  })
+
+}
+
